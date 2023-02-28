b00243f80c24f30881f3156c0dfc3154002e9ad6
Mateus de oliveira <mateus.oliveira.uk@gmail.com>
2023-02-28 17:53:05 +0000

    code added

--- index.js
+++ index.js
@@ -3,19 +3,85 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+// // array of questions for user
+// const questions = [
 
-];
+// ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-// function to initialize program
-function init() {
+// // function to initialize program
+// function init() {
 
-}
+// }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
+
+const promptUser = () => inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please provide installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Please provide contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Please provide test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license does your project have?',
+      choices: ['MIT', 'Apache', 'GPL', 'BSD'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+  ])
+
+  promptUser()
+    .then((data) => {
+        const readme = generateMarkdown(data);
+
+        fs.writeFile('README.md', readme, (err) =>
+        err ? console.error(err) : console.log('README.md generated!')
+        );
+    });
+//     promptUser()
+//   .then((data) => writeFileAsync('index.html', generateHTML(answers)))
+//   .then(() => console.log('Successfully wrote to index.html'))
+
+//   promptUser()
+//   .then((data) => {
+//     console.log(data);
+//   });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,113 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = (data) => {
+  let licenseBadge;
+  if (data.license === 'MIT') {
+    licenseBadge = '![MIT License](https://img.shields.io/badge/License-MIT-green.svg)';
+  } else if (data.license === 'Apache') {
+    licenseBadge = '![Apache License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+  } else if (data.license === 'GPL') {
+    licenseBadge = '![GPL License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)';
+  } else if (data.license === 'BSD') {
+    licenseBadge = '![BSD License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+  }
     return `# ${data.title}
+
+    ## Table of Contents
+  
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Description
+  
+  ${data.description}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  This application is covered under the ${licenseBadge} license.
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  For additional questions, please contact ${data.email}. Visit my [GitHub profile](https://github.com/${data.github}) to see more of my work.
   
   `;
   }
   
   module.exports = generateMarkdown;
   
+
+
+//   const fs = require('fs');
+
+// const generateReadme = (answers) => {
+//   return `# ${answers.title}
+
+//   ## Table of Contents
+  
+//   - [Description](#description)
+//   - [Installation](#installation)
+//   - [Usage](#usage)
+//   - [License](#license)
+//   - [Contributing](#contributing)
+//   - [Tests](#tests)
+//   - [Questions](#questions)
+
+//   ## Description
+  
+//   ${answers.description}
+
+//   ## Installation
+
+//   ${answers.installation}
+
+//   ## Usage
+
+//   ${answers.usage}
+
+//   ## License
+
+//   This application is covered under the ${answers.license} license.
+
+//   ## Contributing
+
+//   ${answers.contributing}
+
+//   ## Tests
+
+//   ${answers.tests}
+
+//   ## Questions
+
+//   For additional questions, please contact ${answers.email}. Visit my [GitHub profile](https://github.com/${answers.github}) to see more of my work.
+//   `;
+// };
+
+// inquirer
+//   .prompt([...])
+//   .then((answers) => {
+//     const readme = generateReadme(answers);
+
+//     fs.writeFile('README.md', readme, (err) =>
+//       err ? console.error(err) : console.log('README.md generated!')
+//     );
+//   });
